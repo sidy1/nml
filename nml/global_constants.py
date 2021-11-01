44052ec52a6e9d59a5da906626b2e044999397c6
glx22 <glx@openttd.org>
2021-11-01 04:17:16 +0100

    Add: global constants for stations

--- nml/global_constants.py
+++ nml/global_constants.py
@@ -280,6 +280,8 @@ constant_numbers = {
     "AIRCRAFT_TYPE_LARGE"      : 0x03,
 
     # ground sprite IDs
+    "GROUNDSPRITE_RAIL_Y"      : 1011,
+    "GROUNDSPRITE_RAIL_X"      : 1012,
     "GROUNDSPRITE_CONCRETE"    : 1420,
     "GROUNDSPRITE_CLEARED"     : 3924,
     "GROUNDSPRITE_GRASS_1_3"   : 3943,
@@ -299,6 +301,35 @@ constant_numbers = {
 
     # general CB for rerandomizing
     "CB_RANDOM_TRIGGER"        : 0x01,
+
+    # station general flags
+    "STAT_FLAG_DISTRIBUTED_CARGO"     : 1,
+    "STAT_FLAG_RANDOM_ANIMATION"      : 2,
+    "STAT_FLAG_CUSTOM_FOUNDATIONS"    : 3,
+    "STAT_FLAG_EXTENDED_FOUNDATIONS"  : 4,
+
+    # station tile types
+    "STAT_TILE_PLAIN_PLATFORM"        : 0,
+    "STAT_TILE_PLAIN_PLATFORM_X"      : 0,
+    "STAT_TILE_PLAIN_PLATFORM_Y"      : 1,
+    "STAT_TILE_PLATFORM_BUILDING"     : 2,
+    "STAT_TILE_PLATFORM_BUILDING_X"   : 2,
+    "STAT_TILE_PLATFORM_BUILDING_Y"   : 3,
+    "STAT_TILE_PLATFORM_ROOF_LEFT"    : 4,
+    "STAT_TILE_PLATFORM_ROOF_LEFT_X"  : 4,
+    "STAT_TILE_PLATFORM_ROOF_LEFT_Y"  : 5,
+    "STAT_TILE_PLATFORM_ROOF_RIGHT"   : 6,
+    "STAT_TILE_PLATFORM_ROOF_RIGHT_X" : 6,
+    "STAT_TILE_PLATFORM_ROOF_RIGHT_Y" : 7,
+
+    # station animation triggers
+    "STAT_ANIM_IS_BUILT"              : 0,
+    "STAT_ANIM_CARGO_ARRIVES"         : 1,
+    "STAT_ANIM_CARGO_REMOVED"         : 2,
+    "STAT_ANIM_TRAIN_ENTERS"          : 3,
+    "STAT_ANIM_TRAIN_LEAVES"          : 4,
+    "STAT_ANIM_TRAIN_LOAD_UNLOAD"     : 5,
+    "STAT_ANIM_256_TICKS"             : 6,
 
     # house flags
     "HOUSE_FLAG_NOT_SLOPED"         : 1,
